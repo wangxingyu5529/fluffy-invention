396740964f4e00b17429ee2ed278bf5a2bae0fc1
wangxingyu5529 <xingyu.wang.97@gmail.com>
2020-09-02 11:12:33 -0500

    Finished rough version of prioritization tool.

--- prioritize_tasks.py
+++ prioritize_tasks.py
@@ -1,34 +1,60 @@
+from time import sleep
 
 
 if __name__ == "__main__":
-	print("So many things to do! But you can get them done by prioritizing! One thing at a time.")
-	print("This is the task list you had:")
+	print("So many things to do! But you can get them done by prioritizing! One thing at a time.\n"); sleep(0.5)
+	print("This is the task list you had: \n"); sleep(1)
 	task_lst = []
+
+	# Read task list
 	with open("tasks.txt", 'r') as f: 
 		tasks = f.readlines()
 		for task in tasks: 
-			print(task)
-			task_lst.append(task)
-	print("Any tasks you would like to cross off?")
+			print(task[:-1]); sleep(0.5)
+			task_lst.append(task[:-1])
+	print("Any tasks you would like to cross off? Yes or No")
 	ans = input()
-	while ans != "No": 
-		print("Which one did you finish? Seperate with comma")
-		nums = list(map(int, input().split(',')))
-		for n in sorted(nums, reverse=True): 
-			task_lst.pop(n-1)
-		print("Here are the tasks again")
-		for task in task_lst: 
-			print(task)
-		print("Any other tasks you would like to cross off?")
-		ans = input()
-	print("Great")
 
-	print("Any tasks you would like to add?")
+	# Delete finished tasks
+	while ans != "No": 
+		print("Which one did you finish? Separate with comma")
+		to_remove = input().split(', ')
+		for x in sorted(to_remove, reverse=True): 
+			if x not in task_lst: 
+				print("There seems to be a typo. Try it again?")
+			else: 
+				task_lst.remove(x)
+		print("I have crossed them off. Here are the tasks again:\n"); sleep(1)
+		for task in task_lst: 
+			print(task); sleep(0.5)
+		print("Any other tasks you would like to cross off? Yes or No")
+		ans = input()
+	print("\nNo problem. \n")
+
+	# Add new tasks to the appropriate place
+	print("Any task you would like to add?")
 	ans = input()
 	while ans != "No":
-		print("What is the name of the task?")
+		print("\nWhat is the name of the task?")
 		name = input()
-		for task in reversed(task_lst):
-			print("Is {} more important than {}".formate(name, task))
+		task_lst.append(name)
+		n = len(task_lst)
+
+		for i in range(n-2, -1, -1):
+			print("Is {} more important than {}".format(name, task_lst[i]))
 			if input() == "Yes":
-				task 
+				task_lst[i+1], task_lst[i] = task_lst[i], task_lst[i+1]
+			else: 
+				break
+		print("We have added {} to the list. Here is your new to-do-list.".format(name))
+
+		for x in task_lst: 
+			print(x); sleep(0.5)
+
+		print("Do you want to add another task? Yes or No")
+		ans = input()
+
+	with open('tasks.txt', 'w') as f: 
+		f.writelines([task+"\n" for task in task_lst])
+
+	print("Great. I have updated your to-do-list.")
