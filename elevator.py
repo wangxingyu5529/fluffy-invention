43da18faa1cf7b126b7218d6d7b6be6addffc5f1
wangxingyu5529 <xingyu.wang.97@gmail.com>
2020-09-06 12:17:04 -0500

    Updated list of tasks and created elevator algorithm python file.

---
+++ elevator.py
@@ -0,0 +1,14 @@
+"""
+I live in a 10-floor building with 2 elevators. 
+Ever since the quarantine, the elevator is limited with
+one person or household per ride. But it wastes a lot of time
+for a person to press the elevator button only to find out that
+the elevator is already occupied (it also wastes the time of the
+person who is already in the elevator). So what is an effective
+algorithm for elevator?
+
+Details to pay attention: 
+- The two elevators will always return to default floors 1 and 5
+respectively when no one requests a ride.
+"""
+
