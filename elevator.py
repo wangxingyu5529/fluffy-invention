3facccc7b2f5ebb614ba3963483b07be48717a42
wangxingyu5529 <xingyu.wang.97@gmail.com>
2020-09-09 11:15:46 -0500

    Finished generate_random_requests function. Modified the design of request and elevator objects.

--- elevator.py
+++ elevator.py
@@ -7,39 +7,80 @@ the elevator is already occupied (it also wastes the time of the
 person who is already in the elevator). So what is an effective
 algorithm/data structure for elevator?
 
-Details to pay attention: 
+The program needs to accomplish the following: 
 - The two elevators will always return to default floors 1 and 5
 respectively when no one requests a ride.
 - If both elevators are free, the one closest to the new request
-should take the order
+should take the order. If an elevator is occupied, it should not
+take any other order, so no two households will share the same
+ride. 
+- Simulate an elevator situation, with random floor request being
+generated and processed
 """
 
 
 # Use queue to arrange incoming ride requests
 
+import random
+
 class Request(object): 
 
-	def __init__(arrival_time, from_floor, to_floor):
-		self.arrival_time = arrival_time
-		self.start_time = None
+	def __init__(self, arrival_time, from_floor, to_floor):
+		self.arrival_time = arrival_time # use integers to represent time here
 		self.from_floor = from_floor
 		self.to_floor = to_floor
 
-class Elevators(object): 
+	def __lt__(self, other):
+		# comparison for generating heap later on
+		return self.arrival_time < other.arrival_time
 
-	def __init__(num_elevators):
-		self.__line = queue.PriorityQueue(maxsize=num_elevators)
+	def __repr__(self):
+		return "Request: arrival_time = {}, from_floor = {}, to_floor = {}".format(
+			self.arrival_time, self.from_floor, self.to_floor)
 
-	def remove_person(self): 
-		return self.__line.get(block=False)[1]
+class Elevator(object): 
 
-	def put_person(self, request):
-		# the priorityqueue sort requests by arrival time
-		return self.__line.put((request.arrival_time, request), block=False)
+	def __init__(default_floor):
+		self.default_floor = default_floor
+		self.occupied = False
 
-	def isfull(self): 
-		return self.__line.full()
-			
+
+def generate_random_requests(num_requests):
+	'''
+	Input: 
+		num_requests: the number of requests that will be generated
+		in this simulation
+	Output: (list) a list of requests objects
+	'''
+
+	requests_floors = random.sample(range(-1, 11), num_requests)
+	# we take the index of requests_floors to be the arrival time
+	requests_lst = []
+	for arrival_time, from_floor in enumerate(requests_floors): 
+		if from_floor == 1: 
+			# if a person enters from floor one, they will only
+			# go to other floors
+			to_floor = random.choice([-1] + list(range(2, 11)))
+		elif from_floor == -1: 
+			to_floor = random.choice(range(1, 11))
+		else: 
+			# if a person did not come from floor 1 or basement, 
+			# this person wants to go to floor 1 or basement
+			to_floor = random.choice([-1, 1])
+		print(arrival_time, from_floor, to_floor)
+		request = Request(arrival_time, from_floor, to_floor)
+		requests_lst.append(request)
+	return requests_lst
+
+
+def simulate(num_requests):
+	'''
+	Input: 
+		num_requests: the number of requests that will be generated
+		in this simulation
+	'''
+	requests_lst = generate_random_requests(num_requests)
+	
 
 
 
