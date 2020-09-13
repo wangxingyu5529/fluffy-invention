c0053cfbae1d47402febe445d781c71bf4ea70a7
wangxingyu5529 <xingyu.wang.97@gmail.com>
2020-09-13 10:21:00 -0500

    Finished a simple version for 7-11 players.

--- werewolf.py
+++ werewolf.py
@@ -1,7 +1,16 @@
 '''
-Recently I've been playing Werewolf with friends over Zoom. 
+Recently I've been playing Werewolf/Mafia with friends over Zoom. 
 This is a program that can automatically assign roles to people.
 '''
+from random import shuffle
+
+role_dict = {
+	7: ['mafia'] * 2 + ['cop', 'doctor'] + ['townies'] * 3,
+	8: ['werewolf'] * 3 + ['prophet', 'witch', 'hunter'] + ['townies'] * 2,
+	9: ['werewolf'] * 3 + ['prophet', 'witch', 'hunter'] + ['townies'] * 3,
+	10: ['werewolf'] * 3 + ['prophet', 'witch', 'hunter'] + ['townies'] * 4,
+	11: ['werewolf'] * 4 + ['prophet', 'witch', 'hunter', 'idiot'] + ['townies'] * 3,
+}
 
 def generate_roles(names):
 	'''
@@ -11,4 +20,6 @@ def generate_roles(names):
 	Output: 
 		list of tuples of the form (name, role)
 	'''
-	
+	n = len(names)
+	shuffle(names)
+	return list(zip(names, role_dict[n]))
